1bbb12bd93d69338f8377b85132f975c070b073a
Vinyas Singh <kumar.v.singh@oracle.com>
2020-08-22 01:14:27 +0530

    Init files

---
+++ src/components/InfoBar/InfoBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import closeIcon from "../../icons/closeIcon.png";
+import onlineIcon from "../../icons/onlineIcon.png";
+
+import "./InfoBar.css";
+
+const InfoBar = ({ room }) => {
+  return (
+    <div className="infoBar">
+      <div className="leftInnerContainer">
+        <img src={onlineIcon} alt="online image" className="onlineIcon" />
+        <h3>{room}</h3>
+      </div>
+      <div className="rightInnerContainer">
+        <a href="/">
+          <img src={closeIcon} alt="close image" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default InfoBar;
---
+++ src/components/Input/Input.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./Input.css";
+
+const Input = ({ message, setMessage, sendMessage }) => {
+  return (
+    <div>
+      <form className="form">
+        <input
+          className="input"
+          type="text"
+          placeholder="Type a message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => (e.key === "Enter" ? sendMessage(e) : null)}
+        />
+        <button className="sendButton" onClick={(e) => sendMessage(e)}>
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Input;
